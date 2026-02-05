a995cf55ffdde64d5c62f42166406ad284c5cecb
nagao.yuki <5319256+zrn-ns@users.noreply.github.com>
2026-02-05 16:01:46 +0900

    Initial Commit

---
+++ NavigationSample/NavigationSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationSampleApp.swift
+//  NavigationSample
+//
+//  Created by nagao.yuki on 2026/02/05.
+//
+
+import SwiftUI
+
+@main
+struct NavigationSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
